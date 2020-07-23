12e874d5a277cc8eaff72b585ebe1ab4d33a94c6
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-23 04:11:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  def initialize 
+  def initialize(persons_name) 
+    @name = persons_name
+  end 
+  
+  def name=(persons_name)
+    @name = persons_name 
+  end 
+  
+  def name
+    @name
+  end 
+  
+end 
     
